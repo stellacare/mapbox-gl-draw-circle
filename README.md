a8d4a6e5b3c8b078c8d91127d234b92e4f22d515
Theis Newton Jensen <theis@stellacare.dk>
2022-06-23 15:01:13 +0200

    Reformat Code

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # mapbox-gl-draw-circle
 
-Adds support for drawing and editing a circle feature using [mapbox-gl-draw](https://github.com/mapbox/mapbox-gl-draw) library.
+Adds support for drawing and editing a circle feature using [mapbox-gl-draw](https://github.com/mapbox/mapbox-gl-draw)
+library.
 
 ## Demo
 
 ##### Circle mode
+
 ![Circle Mode Demo](demo/CircleModeDemo.gif)
 
 ##### Drag Circle mode
-![Drag Circle Mode Demo](demo/DragCircleDemo.gif)
 
+![Drag Circle Mode Demo](demo/DragCircleDemo.gif)
 
 ## Usage
 
@@ -52,9 +54,11 @@ The default radius units are in **kilometers** and initial radius is **2km**.
 draw.changeMode('draw_circle', { initialRadiusInKm: 0.5 });
 ```
 
-It fires the same events as the mapbox-gl-draw library. For more information follow this [link](https://github.com/mapbox/mapbox-gl-draw/blob/master/docs/API.md#events).
+It fires the same events as the mapbox-gl-draw library. For more information follow
+this [link](https://github.com/mapbox/mapbox-gl-draw/blob/master/docs/API.md#events).
 
 Sample feature object returned in `draw.create` event
+
 ```
 {
   "id": "e184898e58feaa5c2c56f20a178ffe2c",
--- __tests__/modes/DirectModeOverride.test.js
+++ __tests__/modes/DirectModeOverride.test.js
@@ -1,10 +1,9 @@
-
 jest.mock('@mapbox/mapbox-gl-draw/src/lib/create_supplementary_points');
 jest.mock('@mapbox/mapbox-gl-draw/src/lib/move_features');
 jest.mock('@mapbox/mapbox-gl-draw/src/lib/constrain_feature_movement');
-jest.mock('@turf/distance', () => ({ default: jest.fn() }));
+jest.mock('@turf/distance', () => ({default: jest.fn()}));
 jest.mock('@turf/helpers');
-jest.mock('@turf/circle', () => ({ default: jest.fn() }));
+jest.mock('@turf/circle', () => ({default: jest.fn()}));
 jest.mock('../../lib/utils/create_supplementary_points_circle');
 
 const createSupplementaryPoints = require('@mapbox/mapbox-gl-draw/src/lib/create_supplementary_points');
@@ -17,89 +16,89 @@ const createSupplementaryPointsForCircle = require('../../lib/utils/create_suppl
 let DirectMode = require('../../lib/modes/DirectModeOverride');
 
 describe('DirectMode tests', () => {
-  let mockState = {};
-  let mockEvent = {};
-  let mockDelta = {};
-  let mockFeatures;
+    let mockState = {};
+    let mockEvent = {};
+    let mockDelta = {};
+    let mockFeatures;
 
-  beforeEach(() => {
-    DirectMode = {
-      ...DirectMode,
-      getSelected: jest.fn(),
-      fireActionable: jest.fn()
-    };
+    beforeEach(() => {
+        DirectMode = {
+            ...DirectMode,
+            getSelected: jest.fn(),
+            fireActionable: jest.fn()
+        };
 
-    mockEvent = {
-      lngLat: { lat: 0, lng: 0 }
-    };
+        mockEvent = {
+            lngLat: {lat: 0, lng: 0}
+        };
 
-    mockDelta = {
-      lat: 1,
-      lng: 1
-    };
-    mockFeatures = [
-      {
-        properties: {
-          isCircle: true,
-          center: [0, 0]
-        },
-        geometry: {
-          coordinates: []
+        mockDelta = {
+            lat: 1,
+            lng: 1
+        };
+        mockFeatures = [
+            {
+                properties: {
+                    isCircle: true,
+                    center: [0, 0]
+                },
+                geometry: {
+                    coordinates: []
+                }
+            }
+        ];
+        mockState = {
+            featureId: 1,
+            feature: {
+                ...mockFeatures[0],
+                incomingCoords: jest.fn()
+            }
         }
-      }
-    ];
-    mockState = {
-      featureId: 1,
-      feature: {
-        ...mockFeatures[0],
-        incomingCoords: jest.fn()
-      }
-    }
-    DirectMode.getSelected.mockReturnValue(mockFeatures);
-  });
+        DirectMode.getSelected.mockReturnValue(mockFeatures);
+    });
 
-  afterEach(() => {
-    createSupplementaryPoints.mockClear();
-    createSupplementaryPointsForCircle.mockClear();
-  });
+    afterEach(() => {
+        createSupplementaryPoints.mockClear();
+        createSupplementaryPointsForCircle.mockClear();
+    });
 
-  it('should move selected features when dragFeature is invoked', () => {
-    DirectMode.dragFeature(mockState, mockEvent, mockDelta);
-    expect(moveFeatures).toHaveBeenCalledWith(mockFeatures, mockDelta);
-  });
+    it('should move selected features when dragFeature is invoked', () => {
+        DirectMode.dragFeature(mockState, mockEvent, mockDelta);
+        expect(moveFeatures).toHaveBeenCalledWith(mockFeatures, mockDelta);
+    });
 
-  it('should update the center of the selected feature if its a circle', () => {
-    DirectMode.dragFeature(mockState, mockEvent, mockDelta);
-    expect(mockFeatures[0].properties.center).toEqual([1, 1]);
-  });
+    it('should update the center of the selected feature if its a circle', () => {
+        DirectMode.dragFeature(mockState, mockEvent, mockDelta);
+        expect(mockFeatures[0].properties.center).toEqual([1, 1]);
+    });
 
-  it('should set dragMoveLocation to the event lngLat', () => {
-    DirectMode.dragFeature(mockState, mockEvent, mockDelta);
-    expect(mockState.dragMoveLocation).toEqual(mockEvent.lngLat);
-  });
+    it('should set dragMoveLocation to the event lngLat', () => {
+        DirectMode.dragFeature(mockState, mockEvent, mockDelta);
+        expect(mockState.dragMoveLocation).toEqual(mockEvent.lngLat);
+    });
 
-  it('should update the radius when dragVertex is invoked and the feature is a circle', () => {
-    distance.mockReturnValue(1);
-    circle.mockReturnValue(mockFeatures[0]);
-    DirectMode.dragVertex(mockState, mockEvent, mockDelta);
-    expect(mockState.feature.incomingCoords).toHaveBeenCalledWith(mockFeatures[0].geometry.coordinates);
-    expect(mockState.feature.properties.radiusInKm).toEqual(1);
-  });
+    it('should update the radius when dragVertex is invoked and the feature is a circle', () => {
+        distance.mockReturnValue(1);
+        circle.mockReturnValue(mockFeatures[0]);
+        DirectMode.dragVertex(mockState, mockEvent, mockDelta);
+        expect(mockState.feature.incomingCoords).toHaveBeenCalledWith(mockFeatures[0].geometry.coordinates);
+        expect(mockState.feature.properties.radiusInKm).toEqual(1);
+    });
 
-  it(`should display points generated using 
+    it(`should display points generated using 
         createSupplementaryPointsForCircle when the feature is a circle`, () => {
-      const mockDisplayFn = jest.fn();
-      const mockGeoJSON = {
-        properties: {
-          id: 1,
-          user_isCircle: true
-        }
-      };
-      createSupplementaryPointsForCircle.mockReturnValue([]);
-      DirectMode.toDisplayFeatures(mockState, mockGeoJSON, mockDisplayFn);
-      expect(mockDisplayFn).toHaveBeenCalledWith(mockGeoJSON);
-      expect(createSupplementaryPointsForCircle).toHaveBeenCalledWith(mockGeoJSON);
-      expect(DirectMode.fireActionable).toHaveBeenCalled();
+        const mockDisplayFn = jest.fn();
+        const mockGeoJSON = {
+            properties: {
+                id: 1,
+                user_isCircle: true
+            }
+        };
+        createSupplementaryPointsForCircle.mockReturnValue([]);
+        DirectMode.toDisplayFeatures(mockState, mockGeoJSON, mockDisplayFn);
+        expect(mockDisplayFn).toHaveBeenCalledWith(mockGeoJSON);
+        expect(createSupplementaryPointsForCircle).toHaveBeenCalledWith(mockGeoJSON);
+        expect(DirectMode.fireActionable).toHaveBeenCalled();
     });
 
     it(`should display points generated using createSupplementaryPoints
@@ -107,26 +106,26 @@ describe('DirectMode tests', () => {
         createSupplementaryPoints.mockReturnValue([]);
         const mockDisplayFn = jest.fn();
         const mockGeoJSON = {
-          properties: {
-            id: 1,
-            user_isCircle: false
-          }
+            properties: {
+                id: 1,
+                user_isCircle: false
+            }
         };
         DirectMode.toDisplayFeatures(mockState, mockGeoJSON, mockDisplayFn);
         expect(mockDisplayFn).toHaveBeenCalledWith(mockGeoJSON);
         expect(createSupplementaryPoints).toHaveBeenCalledWith(mockGeoJSON, {
-          map: undefined, midpoints: true, selectedPaths: undefined
+            map: undefined, midpoints: true, selectedPaths: undefined
         });
         expect(DirectMode.fireActionable).toHaveBeenCalled();
-      });
+    });
 
     it('should not create supplementary vertices if the feature is not selected', () => {
         const mockDisplayFn = jest.fn();
         const mockGeoJSON = {
-          properties: {
-            id: 2,
-            user_isCircle: false
-          }
+            properties: {
+                id: 2,
+                user_isCircle: false
+            }
         };
         DirectMode.toDisplayFeatures(mockState, mockGeoJSON, mockDisplayFn);
         expect(mockDisplayFn).toHaveBeenCalledWith(mockGeoJSON);
